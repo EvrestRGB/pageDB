62ab3ba085a677bfe4fb6bf19d3369575d587522
EvrestRGB <elzadam11@gmail.com>
2023-06-12 19:07:46 +0100

    Update data.php

--- data.php
+++ data.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Vérifier la connexion
 if ($conn->connect_error) {
     die("Erreur de connexion à la base de données : " . $conn->connect_error);
-}
+}   
 
 // Récupérer les données de la base de données
 $sql = "SELECT nom, age FROM utilisateurs";
